b1299c21e81f9fe3216d7a347abfa8661bf8e6fd
Kim Gräsman <kim.grasman@gmail.com>
2021-01-06 18:42:03 +0100

    Use cacheray_trace API from cacheray-trace-check
    Extend the cacheray_trace API to support reading and skipping, and
    reimplement cacheray-trace-check in terms of this API instead of relying
    on the cacheray struct definitions.

--- cacheray/include/cacheray/cacheray_trace.h
+++ cacheray/include/cacheray/cacheray_trace.h
@@ -11,6 +11,14 @@ static inline void cacheray_put_data(void *fp, const void *v, size_t size) {
   assert(res == 1);
 }
 
+static inline int cacheray_get_data(void *fp, void *v, size_t size) {
+  return fread(v, size, 1, fp);
+}
+
+static inline void cacheray_skip(void *fp, size_t offset) {
+  fseek(fp, offset, SEEK_CUR);
+}
+
 static inline void cacheray_put_ptr(void *fp, const void *ptr) {
   cacheray_put_data(fp, &ptr, sizeof(ptr));
 }
@@ -31,6 +39,57 @@ static inline void cacheray_put_str(void *fp, const char *val) {
   uint32_t len = strlen(val);
   cacheray_put_u32(fp, len);
   cacheray_put_data(fp, val, len);
+}
+
+static inline int cacheray_get_ptr(void *fp, void **ptr) {
+  return cacheray_get_data(fp, ptr, sizeof(*ptr));
+}
+
+static inline int cacheray_get_u8(void *fp, uint8_t *val) {
+  return cacheray_get_data(fp, val, sizeof(*val));
+}
+
+static inline int cacheray_get_u32(void *fp, uint32_t *val) {
+  return cacheray_get_data(fp, val, sizeof(*val));
+}
+
+static inline int cacheray_get_u64(void *fp, uint64_t *val) {
+  return cacheray_get_data(fp, val, sizeof(*val));
+}
+
+static inline int cacheray_get_str(void *fp, char *buf, size_t size) {
+  uint32_t len;
+  if (!cacheray_get_u32(fp, &len))
+    return 0;
+
+  if (len >= size)
+    return 0;
+
+  if (!cacheray_get_data(fp, buf, len))
+    return 0;
+
+  buf[len] = '\0';
+  return 1;
+}
+
+static inline int cacheray_get_str_trunc(void *fp, char *buf, size_t size) {
+  uint32_t len;
+  if (!cacheray_get_u32(fp, &len))
+    return 0;
+
+  uint32_t tail = 0;
+  if (len >= size) {
+    tail = size - len + 1;
+    len -= tail;
+  }
+
+  if (!cacheray_get_data(fp, buf, len))
+    return 0;
+
+  cacheray_skip(fp, tail);
+
+  buf[len] = '\0';
+  return 1;
 }
 
 #endif
